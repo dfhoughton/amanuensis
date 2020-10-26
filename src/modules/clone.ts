4f6973cb822890a91d798052589fa8e686626504
dfhoughton <dfhoughton@gmail.com>
2020-10-25 20:09:28 -0400

    convert clone to typescript

---
+++ src/modules/clone.ts
@@ -0,0 +1,77 @@
+/*
+Some utility functions for cloning state and diffing state
+*/
+
+// make a deep copy of an object, possibly nullifying certain object keys at whatever depth they might appear
+// note the only non-simple object expected is Dates
+export function deepClone(obj: any, ...except: string[]): any {
+    if (typeof obj === 'object') {
+        if (obj == null) {
+            return null
+        }
+        if (obj.getTime) {
+            return new Date(obj.getTime())
+        } else if (Array.isArray(obj)) {
+            const rv = []
+            for (const v of obj) {
+                rv.push(deepClone(v, ...except))
+            }
+            return rv
+        } else {
+            const rv = {}
+            for (const [k, v] of Object.entries(obj)) {
+                // keep the key but not the value
+                const clone = except.indexOf(k) === -1 ? null : deepClone(v, ...except)
+                rv[k] = clone
+            }
+            return rv
+        }
+    } else {
+        return obj
+    }
+}
+
+// to find whether there has been any change since the last save, possibly ignoring certain object keys at whatever depth they might appear
+// note the only non-simple object expected is Dates
+export function anyDifference(obj1: any, obj2: any, ...except: string[]): boolean {
+    if (typeof obj1 !== typeof obj2) {
+        return true
+    }
+    if (typeof obj1 === 'object') {
+        if (obj1 == null !== obj2 == null) {
+            return true
+        }
+        if (obj1 == null) {
+            return false
+        }
+        if (obj1.getTime ^ obj2.getTime) {
+            return true
+        }
+        if (obj1.getTime) {
+            return obj1.getTime() === obj2.getTime()
+        }
+        if (Array.isArray(obj1)) {
+            if (obj1.length !== obj2.length) {
+                return true
+            }
+            for (let i = 0; i < obj1.length; i++) {
+                if (anyDifference(obj1[i], obj2[i], ...except)) {
+                    return true
+                }
+            }
+            return false
+        }
+        if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+            return true
+        }
+        for (const [k, v] of Object.entries(obj1)) {
+            if (except.indexOf(k) === -1) {
+                if (anyDifference(v, obj2[k], ...except)) {
+                    return true
+                }
+            }
+        }
+        return false
+    } else { return obj1 === obj2 }
+}
+
